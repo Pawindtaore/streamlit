8e35a78f1051e6b018ae7f0719cbc7055e79f9e8
MaestroDavis <57568252+Pawindtaore@users.noreply.github.com>
2023-02-11 09:24:02 +0000

    hamburg menu hidden

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 import streamlit as st
 import pandas as pd
 
+hide_menu_style = """
+        <style>
+        #MainMenu {visibility: hidden;}
+        </style>
+        """
+st.markdown(hide_menu_style, unsafe_allow_html=True)
+
 st.write("Here's our first attempt at using data to create a table:")
 st.write(pd.DataFrame({
     'first column': [1, 2, 3, 4],
